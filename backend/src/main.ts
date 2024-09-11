bf5f815d693531905c2bb48145ffaf0a3e9ce436
Vinicius José <viniciusjosedev@gmail.com>
2024-09-11 17:05:34 -0300

    feat: Add log when server up

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import log from './log';
 
 async function bootstrap() {
+  const port = process.env.NODE_PORT || 8080;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.NODE_PORT || 8080);
+  await app.listen(port, () => log.info(`Server running in port ${port}`));
 }
 bootstrap();
