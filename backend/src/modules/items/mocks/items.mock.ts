09ebaf6053e10dd3adb25778edb27b379a934d89
Vinicius José <viniciusjosedev@gmail.com>
2024-09-11 17:06:16 -0300

    feat: Mock file for population bd in test cases

---
+++ backend/src/modules/items/mocks/items.mock.ts
@@ -0,0 +1,26 @@
+const itemsMock = [
+  {
+    id: '66e1e5a06bc6cdb79dfd2143',
+    name: 'P250 | Inferno (Well-Worn)',
+    image:
+      'https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_p250_gs_p250_inferno_medium_png.png',
+    category: 'Pistols',
+    float: '0.68',
+    price: 2,
+    createdAt: '2024-09-11T18:46:56.333Z',
+    updatedAt: '2024-09-11T18:46:56.333Z',
+  },
+  {
+    id: '66e1e5a06bc6cdb79dfd260d',
+    name: 'Souvenir USP-S | Road Rash (Minimal Wear)',
+    image:
+      'https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_usp_silencer_cu_usp_sandpapered_light_png.png',
+    category: 'Pistols',
+    float: '1',
+    price: 500,
+    createdAt: '2024-09-11T18:46:56.333Z',
+    updatedAt: '2024-09-11T18:46:56.333Z',
+  },
+];
+
+export default itemsMock;
