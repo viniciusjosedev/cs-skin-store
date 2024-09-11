9eda5e07a8bd3d9088d4caf5e5e3c5faf5cc4927
Vinicius José <viniciusjosedev@gmail.com>
2024-09-11 17:11:40 -0300

    feat: add cors config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,16 @@ import log from './log';
 async function bootstrap() {
   const port = process.env.NODE_PORT || 8080;
 
-  const app = await NestFactory.create(AppModule);
+  const urlsPermitted = process.env.FRONTEND_URL
+    ? [process.env.FRONTEND_URL]
+    : '*';
+
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: urlsPermitted,
+      credentials: true,
+    },
+  });
   await app.listen(port, () => log.info(`Server running in port ${port}`));
 }
 bootstrap();
